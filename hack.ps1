cab7d41a6b632e00b747e570755e987d46cfe762
Jiale Zhu <jialzh@microsoft.com>
2017-12-31 15:37:37 +0800

    Speed up

--- hack.ps1
+++ hack.ps1
@@ -5,15 +5,17 @@ cd $workpath
 do {
     Write-Host "Ready Go!"
     # Capture screenshot
-    adb shell screencap -p /sdcard/screenshot.png;
+    
     copy screenshot-2.png screenshot-3.png
     copy screenshot-1.png screenshot-2.png
     copy screenshot.png screenshot-1.png
-    adb pull /sdcard/screenshot.png $workpath;
+    Write-Host "Captrue......"
+    sh cap.sh
+    Write-Host "Captrued"
 
     .\PictureProcess\PictureProcess\obj\Release\PictureProcess.exe .\screenshot.png
     # swipe
     adb shell input swipe 500 1700 500 1700 $LASTEXITCODE
     #adb shell input swipe 0 0 0 0 $LASTEXITCODE
-    Start-Sleep -Seconds 1
+    Start-Sleep -Seconds 1.5
 } while ($true)
